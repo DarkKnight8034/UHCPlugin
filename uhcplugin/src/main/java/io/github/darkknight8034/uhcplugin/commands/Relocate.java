2173e7d2aa5924096e2bb7cc169be2830ba28ca0
DarkKnight8034 <darkknight8034@gmail.com>
2021-11-07 09:41:27 -0500

    relocate command, ready for testing

--- uhcplugin/src/main/java/io/github/darkknight8034/uhcplugin/commands/Relocate.java
+++ uhcplugin/src/main/java/io/github/darkknight8034/uhcplugin/commands/Relocate.java
@@ -5,6 +5,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import org.bukkit.entity.Player;
+import org.bukkit.World;
+import org.bukkit.Location;
+
+import java.util.Random;
 
 import io.github.darkknight8034.uhcplugin.Main;
 
@@ -12,6 +16,7 @@ public class Relocate implements CommandExecutor
 {
     
     private Main plugin;
+    private Random random = new Random();
 
     public Relocate(Main plugin)
     {
@@ -28,12 +33,22 @@ public class Relocate implements CommandExecutor
         Player player = (Player) sender;
         player.sendMessage(args);
 
-        // for (Player p : this.plugin.getServer().getOnlinePlayers())
-        // {
+        World world = player.getWorld();
+        Integer range = Integer.parseInt(args[0]);
 
-            
+        for (Player p : this.plugin.getServer().getOnlinePlayers())
+        {
 
-        // }
+            double x = (double) random.nextInt(range);
+            double z = (double) random.nextInt(range);
+            double y = (double) world.getHighestBlockYAt((int) x, (int) z);
+
+            Location location = new Location(world, x, y, z);
+            p.teleport(location);
+            p.sendMessage("Teleported you to: " + x + ", " + z);
+
+
+        }
 
         return false;
 
